00fda553a04d4f8f227dcbb8294edb649fa29725
Premtim <premtim.berisha@hotmail.com>
2022-03-03 19:39:43 +0100

    fixing

--- server.js
+++ server.js
@@ -25,13 +25,13 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-app.get('*', (req, res)=> { res.send('it is working!') })
+app.get('/', (req, res)=> { res.send('it is working!') })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 8080, ()=> {
+app.listen(process.env.PORT || 3000, ()=> {
   console.log(`app is running on port ${process.env.PORT}`);
 })
